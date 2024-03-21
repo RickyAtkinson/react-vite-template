3851e42fd45df10f8a3a169b49166254a6ec523b
RickyAtkinson <ricky@kinson.digital>
2024-03-21 23:37:31 +0000

    Add prettier and update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,28 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, node: true },
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
+    "prettier",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: ["./tsconfig.json", "./tsconfig.node.json"],
+    tsconfigRootDir: __dirname,
+  },
+  plugins: ["react", "react-refresh"],
   rules: {
+    "react/react-in-jsx-scope": 0,
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
