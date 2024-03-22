96fbaf6218c76c41c7a39bb9599d5f7f5fbe9228
RickyAtkinson <ricky@kinson.digital>
2024-03-22 19:56:01 +0000

    Change export method for App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
-function App() {
+export default function App() {
   const [count, setCount] = useState(0);
 
   return (
@@ -51,5 +51,3 @@ function App() {
     </>
   );
 }
-
-export default App;
