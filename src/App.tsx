f7dd01caaac54c8d60c5c7d071efc6667f677068
RickyAtkinson <ricky@kinson.digital>
2024-03-22 05:47:22 +0000

    Add TaillwindCSS and update example files

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,51 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+      <div className="flex justify-center gap-x-6 p-6">
+        <a
+          className="font-medium text-indigo-600 hover:text-indigo-400"
+          href="https://vitejs.dev"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src={viteLogo}
+            className="logo h-24 transition duration-300 hover:drop-shadow-[0_0_2em_#646cffaa]"
+            alt="Vite logo"
+          />
         </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a
+          className="font-medium text-indigo-600 hover:text-indigo-400"
+          href="https://react.dev"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img
+            src={reactLogo}
+            className="logo react h-24 transition duration-300 hover:drop-shadow-[0_0_2em_#646cffaa]"
+            alt="React logo"
+          />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className="mb-8 text-5xl">Vite React Template</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button
+          className="border-1 mb-5 rounded-lg border border-solid border-transparent bg-stone-950 px-5 py-2 text-lg transition duration-200 hover:border-indigo-400"
+          onClick={() => setCount((count) => count + 1)}
+        >
           count is {count}
         </button>
-        <p>
+        <p className="mb-2">
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="text-neutral-400">
         Click on the Vite and React logos to learn more
       </p>
     </>
