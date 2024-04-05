498bd831fc0a9d5eaee2da1570d761e26acfbd65
RickyAtkinson <ricky@kinson.digital>
2024-04-05 20:12:13 +0100

    Update demo styles

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ export default function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div className="flex justify-center gap-x-6 p-6">
+    <div className="flex min-h-dvh min-w-80 flex-col justify-center">
+      <div className="mb-8 flex justify-center gap-x-10">
         <a
           className="font-medium text-indigo-600 hover:text-indigo-400"
           href="https://vitejs.dev"
@@ -16,7 +16,7 @@ export default function App() {
         >
           <img
             src={viteLogo}
-            className="logo h-24 transition duration-300 hover:drop-shadow-[0_0_2em_#646cffaa]"
+            className="logo h-24 animate-pulse transition duration-300 hover:drop-shadow-[0_0_2em_#646cffaa]"
             alt="Vite logo"
           />
         </a>
@@ -28,26 +28,28 @@ export default function App() {
         >
           <img
             src={reactLogo}
-            className="logo react h-24 transition duration-300 hover:drop-shadow-[0_0_2em_#646cffaa]"
+            className="logo react h-24 animate-[spin_16s_linear_infinite] transition duration-300 hover:drop-shadow-[0_0_2em_#646cffaa]"
             alt="React logo"
           />
         </a>
       </div>
-      <h1 className="mb-8 text-5xl">Vite React Template</h1>
-      <div className="card">
+      <h1 className="mb-8 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Vite React Template
+      </h1>
+      <div>
         <button
-          className="border-1 mb-5 rounded-lg border border-solid border-transparent bg-stone-950 px-5 py-2 text-lg transition duration-200 hover:border-indigo-400"
+          className="border-1 mb-8 rounded-lg border border-solid border-transparent bg-zinc-800 px-5 py-2 text-lg transition duration-200 hover:border-indigo-400"
           onClick={() => setCount((count) => count + 1)}
         >
-          count is {count}
+          Count is {count}
         </button>
-        <p className="mb-2">
+        <p className="eading-7 [&:not(:last-child)]:mb-6">
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
+        <p className="leading-7 text-zinc-400 [&:not(:last-child)]:mb-6">
+          Click on the Vite and React logos to learn more
+        </p>
       </div>
-      <p className="text-neutral-400">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
